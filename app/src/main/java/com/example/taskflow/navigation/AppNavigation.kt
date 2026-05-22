a04441c8a54fb8fd5b878b35cf1d9cd3af8c67b4
Jose Cano <jgcano@uamv.edu.ni>
2026-05-21 23:04:17 -0600

    Subo appnavigation y routes

--- app/src/main/java/com/example/taskflow/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/taskflow/navigation/AppNavigation.kt
@@ -1,2 +1,55 @@
 package com.example.taskflow.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.taskkflow.screen.SplashScreen
+import com.example.taskkflow.screen.TaskDetailScreen
+import com.example.taskkflow.screen.TaskFormScreen
+import com.example.taskkflow.screen.TaskListScreen
+import com.example.taskkflow.viewmodel.TaskViewModel
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    val taskViewModel: TaskViewModel = viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.Splash
+    ) {
+        composable(Routes.Splash) {
+            SplashScreen(navController)
+        }
+
+        composable(Routes.TaskList) {
+            TaskListScreen(
+                navController = navController,
+                viewModel = taskViewModel
+            )
+        }
+
+        composable(Routes.TaskForm) {
+            TaskFormScreen(
+                navController = navController,
+                viewModel = taskViewModel
+            )
+        }
+
+        composable(Routes.TaskDetail) { backStackEntry ->
+            val taskId = backStackEntry.arguments
+                ?.getString("taskId")
+                ?.toIntOrNull()
+
+            if (taskId != null) {
+                TaskDetailScreen(
+                    navController = navController,
+                    taskId = taskId,
+                    viewModel = taskViewModel
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/taskflow/navigation/Routes.kt
+++ app/src/main/java/com/example/taskflow/navigation/Routes.kt
@@ -1,2 +1,9 @@
 package com.example.taskflow.navigation
 
+
+object Routes {
+    const val Splash = "splash"
+    const val TaskList = "taskList"
+    const val TaskForm = "taskForm"
+    const val TaskDetail = "taskDetail/{taskId}"
+}
